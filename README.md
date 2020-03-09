7270d86ad86380828d7d369f0d26045aeddcab17
bublinec <jurajbublinec@gmail.com>
2020-03-09 10:46:15 +0000

    Removing items done

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Shopping List
 
-Hi there!
+Hi There!
+
 This is my first project created using **jQuery** library.
 
 ### This app is published at: https://bublinec.github.io/Shopping-List
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-alert("CONNECTED!")
+// Check off the item on click
+$("li").on("click", function(){
+    $(this).toggleClass("done");
+});
+
+// Remove the intem on button click
+$(".del").on("click", function(event){
+    $(this).parent().fadeOut(300, function(){
+        $(this).remove();
+    });    
+    event.stopPropagation();
+})
+
