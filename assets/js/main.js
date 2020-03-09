075cb6a8d4cd7a437ccee38f87df29834fcebeea
bublinec <jurajbublinec@gmail.com>
2020-03-09 11:03:21 +0000

    Add items done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,22 @@
 // Check off the item on click
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("done");
 });
 
-// Remove the intem on button click
-$(".del").on("click", function(event){
+// Remove the item on button click
+$("ul").on("click", ".del", function(event){
     $(this).parent().fadeOut(300, function(){
         $(this).remove();
     });    
     event.stopPropagation();
+})
+
+// Add new item
+$("input").on("keypress", function(event){
+    if(event.which === 13){
+        input_value = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span class='del'>X</span>" + input_value + "</li>");
+    }
 })
 
